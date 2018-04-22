86d9cb60e57f34dc31aab431110338f01f7e5ff7
gaojin <1019855680@qq.com>
2018-04-23 03:32:28 +0800

    fix nextpage url

--- icospider/spiders/tokens.py
+++ icospider/spiders/tokens.py
@@ -7,6 +7,10 @@ class TokensSpider(scrapy.Spider):
     allowed_domains = ['etherscan.io']
     start_urls = ['http://etherscan.io/tokens/']
     addressUrl = 'https://etherscan.io/address/{}'
+    nextPageUrl = 'https://etherscan.io/{}'
+    custom_settings = {
+        "AUTOTHROTTLE_ENABLED": True,
+    }
 
     def parse(self, response):
         tokens = response.css('a').re('/token/(0x\w+)')
@@ -18,11 +22,11 @@ class TokensSpider(scrapy.Spider):
 
         nextpage = response.xpath('//a[text()="Next"]/@href').extract_first()
         if nextpage:
-            yield response.follow(nextpage)
+            yield scrapy.Request(self.nextPageUrl.format(nextpage, callback=self.parse))
 
     def getCode(self, response):
-        name = response.xpath(
+        name=response.xpath(
             '//a[@data-placement="bottom"]/text()').extract_first()
-        address = response.meta['address']
-        code = response.css('#editor::text').extract_first()
+        address=response.meta['address']
+        code=response.css('#editor::text').extract_first()
         yield {'name': name, 'address': address, 'code': code}
