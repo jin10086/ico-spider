7e8028d0de0fc02bdd292757630211c04698e375
gaojin <igaojin@qq.com>
2018-09-22 11:47:37 +0800

    add user agent

--- icospider/settings.py
+++ icospider/settings.py
@@ -16,7 +16,9 @@ NEWSPIDER_MODULE = "icospider.spiders"
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-# USER_AGENT = 'icospider (+http://www.yourdomain.com)'
+USER_AGENT = (
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"
+)
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
